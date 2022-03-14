3e81a02f12b82d1399e9703e7b1f81459d871fbf
lmnhe <ljymyys@outlook.com>
2022-03-15 02:59:20 +0800

    添加 禁止频道切换。 添加 禁止表情回应。
    Signed-off-by: lmnhe <ljymyys@outlook.com>

---
+++ app/src/main/java/cc/ioctl/tmoe/hook/func/ProhibitChannelSwitching.kt
@@ -0,0 +1,34 @@
+package cc.ioctl.tmoe.hook.func
+
+import cc.ioctl.tmoe.hook.base.CommonDynamicHook
+import com.github.kyuubiran.ezxhelper.utils.*
+
+object ProhibitChannelSwitching : CommonDynamicHook() {
+    override fun initOnce(): Boolean = tryOrFalse {
+        val cpd= loadClass("org.telegram.ui.ChatPullingDownDrawable")
+        for (method in cpd.declaredMethods) {
+            when (method.name) {
+                "getNextUnreadDialog",
+                "drawBottomPanel",
+                "draw"->{
+                    method.hookBefore {
+                        if (isEnabled) it.result=null
+                    }
+                }
+                "showBottomPanel"->{
+                    method.hookBefore {
+                        if (isEnabled) it.args[0]=false
+                    }
+                }
+                "needDrawBottomPanel"->{
+                    method.hookBefore {
+                        if (isEnabled) it.result=false
+                    }
+                }
+            }
+        }
+
+
+
+    }
+}
