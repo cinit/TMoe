238d4e9b08f6b773d85015ea32a4602ea8280330
lmnhe <ljymyys@outlook.com>
2022-03-15 01:17:37 +0800

    添加 禁止频道切换 添加 禁止表情回应 添加 反反复制

---
+++ app/src/main/java/cc/ioctl/tmoe/hook/func/ProhibitEnableReactions.kt
@@ -0,0 +1,21 @@
+package cc.ioctl.tmoe.hook.func
+
+import cc.ioctl.tmoe.hook.base.CommonDynamicHook
+import com.github.kyuubiran.ezxhelper.utils.*
+import java.util.*
+import kotlin.collections.ArrayList
+
+object ProhibitEnableReactions : CommonDynamicHook() {
+    override fun initOnce(): Boolean = tryOrFalse {
+        //禁止表情回应
+        val enabledReactionsList: List<Objects> = ArrayList()
+        findMethod("org.telegram.messenger.MediaDataController"){ name=="getEnabledReactionsList" }.hookBefore {
+            if (isEnabled) it.result=enabledReactionsList
+        }
+
+        findMethod("org.telegram.ui.ChatActivity"){ name=="selectReaction" }.hookBefore {
+            if (isEnabled) it.result = null
+        }
+
+    }
+}
