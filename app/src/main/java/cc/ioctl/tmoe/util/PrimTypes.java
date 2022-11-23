c53114e40aeea7e43a94b929b0d38e7202c693b3
ACh Sulfate <xenonhydride@gmail.com>
2022-11-23 16:03:11 +0800

    chore: add primitive type util

---
+++ app/src/main/java/cc/ioctl/tmoe/util/PrimTypes.java
@@ -0,0 +1,18 @@
+package cc.ioctl.tmoe.util;
+
+public class PrimTypes {
+
+    private PrimTypes() {
+    }
+
+    public static final Class<Integer> INT = int.class;
+    public static final Class<Long> LONG = long.class;
+    public static final Class<Short> SHORT = short.class;
+    public static final Class<Byte> BYTE = byte.class;
+    public static final Class<Float> FLOAT = float.class;
+    public static final Class<Double> DOUBLE = double.class;
+    public static final Class<Boolean> BOOLEAN = boolean.class;
+    public static final Class<Character> CHAR = char.class;
+    public static final Class<Void> VOID = void.class;
+
+}
