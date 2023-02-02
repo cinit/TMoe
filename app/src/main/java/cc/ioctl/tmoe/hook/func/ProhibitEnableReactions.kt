6a853aae7817586e80562312ab231af6c78ab6c1
SUMUS <ljymyys@outlook.com>
2023-02-03 01:09:28 +0800

    Fix:ProhibitEnableReactions

--- app/src/main/java/cc/ioctl/tmoe/hook/func/ProhibitEnableReactions.kt
+++ app/src/main/java/cc/ioctl/tmoe/hook/func/ProhibitEnableReactions.kt
@@ -1,37 +1,47 @@
 package cc.ioctl.tmoe.hook.func
 
+import android.content.Context
+import android.widget.Toast
 import cc.ioctl.tmoe.hook.base.CommonDynamicHook
 import com.github.kyuubiran.ezxhelper.utils.*
 import java.util.*
-import kotlin.collections.ArrayList
 
 object ProhibitEnableReactions : CommonDynamicHook() {
     override fun initOnce(): Boolean = tryOrLogFalse {
+
         val enabledReactionsList: List<Objects> = ArrayList()
         findMethod("org.telegram.messenger.MediaDataController") { name == "getEnabledReactionsList" }.hookBefore {
             if (isEnabled) it.result = enabledReactionsList
         }
 
-        //private void selectReaction(MessageObject primaryMessage, ReactionsContainerLayout reactionsLayout, float x, float y, TLRPC.TL_availableReaction reaction, boolean fromDoubleTap, boolean bigEmoji) {
+
+// old:   private void selectReaction(MessageObject primaryMessage, ReactionsContainerLayout reactionsLayout, float x, float y, TLRPC.TL_availableReaction reaction, boolean fromDoubleTap, boolean bigEmoji) {
+//        private void selectReaction(MessageObject primaryMessage, ReactionsContainerLayout reactionsLayout, View fromView, float x, float y, ReactionsLayoutInBubble.VisibleReaction visibleReaction, boolean fromDoubleTap, boolean bigEmoji, boolean addToRecent) {
         findMethod("org.telegram.ui.ChatActivity") { name == "selectReaction" }.hookBefore {
             if (!isEnabled) return@hookBefore
 
             it.result = null
-            val fromDoubleTap = it.args[5] as Boolean
+//            Log.d(Arrays.toString(it.args))
 
-            if (fromDoubleTap) {
+            val fromDoubleTap = it.args[6] as Boolean
 
-                val BulletinFactory = findMethod("org.telegram.ui.Components.BulletinFactory") {
-                    name == "of" && parameterTypes.size == 1
-                }.invoke(null, it.thisObject)
+            if (fromDoubleTap ) {
 
-                val createCopyBulletin = findMethod(BulletinFactory::class.java) {
-                    name == "createCopyBulletin" && parameterTypes.size == 1
-                }.invoke(BulletinFactory, "双击 666")
+//                val BulletinFactory = findMethod("org.telegram.ui.Components.BulletinFactory") {
+//                    name == "of" && parameterTypes.size == 1
+//                }.invoke(null, it.thisObject)
+//
+//                val createCopyBulletin = findMethod(BulletinFactory::class.java) {
+//                    name == "createCopyBulletin" && parameterTypes.size == 1
+//                }.invoke(BulletinFactory, "双击 666")
+//
+//                findMethod(createCopyBulletin::class.java) {
+//                    name == "show" && parameterTypes.size == 0
+//                }.invoke(createCopyBulletin)
 
-                findMethod(createCopyBulletin::class.java) {
-                    name == "show" && parameterTypes.size == 0
-                }.invoke(createCopyBulletin)
+                //org.telegram.messenger.ApplicationLoader applicationContext
+                val applicationContext = findField("org.telegram.messenger.ApplicationLoader") { name == "applicationContext" }.get(null) as Context
+                Toast.makeText(applicationContext,"双击 666",Toast.LENGTH_SHORT).show()
 
 
             }
